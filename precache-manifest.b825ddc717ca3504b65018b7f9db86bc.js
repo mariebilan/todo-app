9c28019465f8d49720865cca8068a3b7d7a8517e
mariebilan <marie.bilan.cg@gmail.com>
2019-03-17 00:04:52 +0200

    Updates

---
+++ precache-manifest.b825ddc717ca3504b65018b7f9db86bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  },
+  {
+    "revision": "52a6e0d6127e2219c775",
+    "url": "/todo-app/static/js/main.74ec8d0a.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/todo-app/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "52a6e0d6127e2219c775",
+    "url": "/todo-app/static/css/main.748b7433.chunk.css"
+  },
+  {
+    "revision": "ae916ee1710bdec267181d3ee95722db",
+    "url": "/todo-app/index.html"
+  }
+];
