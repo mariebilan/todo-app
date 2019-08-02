bc445ac61237d6cfc25c6b16cadea3f7966627ca
mariebilan <marie.bilan.cg@gmail.com>
2019-08-02 20:33:23 +0300

    management app

---
+++ build2/precache-manifest.2a870174a54a6d800f93c0e218c3d7a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0555af0d0302f662e6a",
+    "url": "/react-hooks/static/js/runtime~main.c0555af0.js"
+  },
+  {
+    "revision": "12cc29cfb9fcf73041f2",
+    "url": "/react-hooks/static/js/main.12cc29cf.chunk.js"
+  },
+  {
+    "revision": "daaf546f5bda08ee30ee",
+    "url": "/react-hooks/static/js/1.daaf546f.chunk.js"
+  },
+  {
+    "revision": "12cc29cfb9fcf73041f2",
+    "url": "/react-hooks/static/css/main.f2f77677.chunk.css"
+  },
+  {
+    "revision": "f3cdb0534eb4f5e04f7d4c5b89c9db0b",
+    "url": "/react-hooks/index.html"
+  }
+];
